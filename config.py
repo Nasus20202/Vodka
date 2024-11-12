ae70d9c04721a88d8c244e44d6d082ee139b39a2
Stanisław Nieradko <stanislaw@nieradko.com>
2024-11-12 09:32:01 +0100

    add funny command

--- config.py
+++ config.py
@@ -10,3 +10,10 @@ if len(sys.argv) > 1:
     token = sys.argv[1]
 
 user_metadata_path = Path("./data/user_metadata.json")
+
+# List of default better users (ketrab20, Nasus, KanarekLife)
+better_users = (
+    os.getenv("BETTER_USERS").split(",")
+    if os.getenv("BETTER_USERS")
+    else [374952720295133184, 374952720295133184, 296347644081471502]
+)
